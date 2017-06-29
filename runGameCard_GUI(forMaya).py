2abcc8c5a2729bd2d8ee9e2f1a4edc4952c5b3d3
jatumin01 <jatumin01@gmail.com>
2017-06-29 12:02:13 +0700

    add runformaya

---
+++ runGameCard_GUI(forMaya).py
@@ -0,0 +1,6 @@
+import sys
+if not "C:/Users/eur/Documents/Git/pipelinew4" in sys.path:
+    sys.path.append("C:/Users/eur/Documents/Git/pipelinew4")
+
+import pipelineGameCard.CG_masterCompiled
+reload(pipelineGameCard.CG_masterCompiled)
